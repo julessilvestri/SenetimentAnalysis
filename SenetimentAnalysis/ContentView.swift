7b67f60a3fb22720cd744ba9ae04f6c0744b6036
julessilvestri <jules.sil05@gmail.com>
2024-01-26 10:26:13 +0100

    Initial Commit

---
+++ SenetimentAnalysis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SenetimentAnalysis
+//
+//  Created by Jules SILVESTRI on 1/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SenetimentAnalysis/SenetimentAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  SenetimentAnalysisApp.swift
+//  SenetimentAnalysis
+//
+//  Created by Jules SILVESTRI on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SenetimentAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
